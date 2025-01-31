c06007c0e3305838416a63b06f9576311364b05e
mafutka <mafutka@gmail.com>
2025-01-31 10:35:18 +0200

    task-4-edited

--- js/task-4.js
+++ js/task-4.js
@@ -1,17 +1,24 @@
 function getShippingCost(country) {
-    const price = {
-    "China": 100,
-    "Chile": 250,
-    "Australia": 170,
-    "Jamaica": 120
+    let price;
+    switch(country) {
+        case "China":
+            price = 100;
+            break;
+        case "Chile":
+            price = 250;
+            break;
+        case "Australia":
+            price = 170;
+            break;
+        case "Jamaica":
+            price = 120;
+            break;
+            default:
+        return "Sorry, there is no delivery to your country";
     }
-    if (price[country]) {
-    return `Shipping to ${country} will cost ${price[country]} credits`;
+    return `Shipping to ${country} will cost ${price} credits`;
 }
-else {
-    return "Sorry, there is no delivery to your country";
-}
-}
+
 
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
